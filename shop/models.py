7eab8a79ad55c02a85fb688fb9de4d88c800765c
Md. Ashikur Rahman Shuvo <shuvo15-5633@diu.edu.bd>
2025-11-17 01:33:24 +0600

    Added models for My Shop

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model): #inheriting the models.Model class
+    product_id=models.AutoField
+    product_name=models.CharField(max_length=50)
+    desc=models.CharField(max_length=300)
+    pub_date=models.DateField()
+
