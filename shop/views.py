dba4bc7566e86f6279f43c4f6af56a7e8ce9b1b5
Md. Ashikur Rahman Shuvo <shuvo15-5633@diu.edu.bd>
2025-11-17 00:28:14 +0600

    Add pages: contact, search, productview, tracker, checkout, about

--- shop/views.py
+++ shop/views.py
@@ -2,3 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
     return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("This is About Page of My Shop")
+
+def contact(request):
+    return HttpResponse("This is Contact Page")
+
+def tracker(requsest):
+    return HttpResponse("This is Tracker Page")
+
+def search(requsest):
+    return HttpResponse("This is Search Page")
+
+def productview(requsest):
+    return HttpResponse("This is Product View Page")
+
+def checkout(request):
+    return HttpResponse("This is Checkout Page")
